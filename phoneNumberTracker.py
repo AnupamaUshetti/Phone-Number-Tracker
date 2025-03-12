5b7c63cbeea664af2965f1af73d8c7d52b0f4980
Anupama Ushetti <anupamaush69@gmail.com>
2025-03-12 17:13:05 +0530

    Updated the flies to track the latitude and the longitude.

--- phoneNumberTracker.py
+++ phoneNumberTracker.py
@@ -3,6 +3,8 @@ from phonenumbers import geocoder, carrier
 from phoneNumbers import number
 import folium
 
+key = "ee4ac5ef92d94272a37530e8c6a98bfd"
+
 check_number = phonenumbers.parse(number)
 number_location = geocoder.description_for_number(check_number, "en")
 print(number_location)
@@ -10,3 +12,34 @@ print(number_location)
 service_provider = phonenumbers.parse(number)
 print(carrier.name_for_number(service_provider, "en"))
 
+from opencage.geocoder import OpenCageGeocode
+geocoder = OpenCageGeocode(key)
+
+query = str(number_location)
+reults = geocoder.geocode(query)
+
+import phonenumbers
+from phonenumbers import geocoder, carrier
+from phoneNumbers import number
+import folium
+
+key = "ee4ac5ef92d94272a37530e8c6a98bfd"
+
+check_number = phonenumbers.parse(number)
+number_location = geocoder.description_for_number(check_number, "en")
+print(number_location)
+
+service_provider = phonenumbers.parse(number)
+print(carrier.name_for_number(service_provider, "en"))
+
+from opencage.geocoder import OpenCageGeocode
+geocoder = OpenCageGeocode(key)
+
+query = str(number_location)
+reults = geocoder.geocode(query)
+
+latitude = reults[0]['geometry']['lat']
+longitude = reults[0]['geometry']['lng']
+print(latitude, longitude)
+
+map_location = folium.Map(location = [lat, lng], zoom_start=9)
