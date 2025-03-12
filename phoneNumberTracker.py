6794408c0777058b7969fe5ba20411508ad80a35
Anupama Ushetti <anupamaush69@gmail.com>
2025-03-12 16:18:06 +0530

    Updated files to track the service provider.

--- phoneNumberTracker.py
+++ phoneNumberTracker.py
@@ -1,9 +1,12 @@
 import phonenumbers
-from phonenumbers import geocoder
+from phonenumbers import geocoder, carrier
 from phoneNumbers import number
 import folium
 
 check_number = phonenumbers.parse(number)
 number_location = geocoder.description_for_number(check_number, "en")
 print(number_location)
+
+service_provider = phonenumbers.parse(number)
+print(carrier.name_for_number(service_provider, "en"))
 
